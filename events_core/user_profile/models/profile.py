81ddd925ba090d47f1c02c5c7a279bbb3e91b737
Andreas Papaconstantinou <andreas.papaconstantinou@mindgeek.com>
2023-12-30 18:23:20 +0200

    make lint after project.toml updates

--- events_core/user_profile/models/profile.py
+++ events_core/user_profile/models/profile.py
@@ -6,9 +6,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     title = models.CharField(blank=True, max_length=150)
     bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/',
-                                        blank=True,
-                                        null=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/', blank=True, null=True
+    )
 
     def __str__(self):
         return self.user.username
